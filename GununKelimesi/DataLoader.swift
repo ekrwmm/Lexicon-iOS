90c11be9a5b2101651f55a350511c4c28b913c7f
ekrem emre buyukcoskun <ekrem.emre.buyukcoskun@gmail.com>
2026-01-11 02:22:57 +0300

    yarıda kalan dosyalar eklendi (ya da sadece ben öyle düşünüyorum). eksikler- app fotoğrafı bildirim saati ayarlanması

---
+++ GununKelimesi/DataLoader.swift
@@ -0,0 +1,8 @@
+//
+//  DataLoader.swift
+//  GununKelimesi
+//
+//  Created by ekrem emre büyükcoşkun on 11.01.2026.
+//
+
+import Foundation
---
+++ GununKelimesi/NotificationManager.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationManager.swift
+//  GununKelimesi
+//
+//  Created by ekrem emre büyükcoşkun on 11.01.2026.
+//
+
+import Foundation
